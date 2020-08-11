3e096e0734a14cebca8c09cf4d6c0dde098b0749
Mohamed Ndiaye <38785512+mohamedndiayedev@users.noreply.github.com>
2020-08-11 03:09:14 +0100

    Update connection.php

--- dashboard/credit/connection.php
+++ dashboard/credit/connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername="localhost";
-$username="root";
-$password="";
-$dbname="skgambia";
+$servername="mkorvuw3sl6cu9ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username="iigi5330azzahj3c";
+$password="smij1gf9l7l65kwi";
+$dbname="jj0m1ku5w7a37l39";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
