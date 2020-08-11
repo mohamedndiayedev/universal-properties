d1e0ec86c713949fcac0332d20a633a9a4d57f3c
Mohamed Ndiaye <38785512+mohamedndiayedev@users.noreply.github.com>
2020-08-11 03:32:06 +0100

    Update db_connect.php

--- libs/db_connect.php
+++ libs/db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost","root","","skgambia");
+$con = mysqli_connect("mkorvuw3sl6cu9ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","iigi5330azzahj3c","smij1gf9l7l65kwi","jj0m1ku5w7a37l39");
 // Check connection
 if (mysqli_connect_errno())
   {
