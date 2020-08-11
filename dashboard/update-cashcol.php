5d284914dd9ba802a454f2d46cd7b8612121b419
Mohamed Ndiaye <38785512+mohamedndiayedev@users.noreply.github.com>
2020-08-11 03:18:46 +0100

    Update update-cashcol.php

--- dashboard/update-cashcol.php
+++ dashboard/update-cashcol.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$connection = mysqli_connect("localhost","root","","skgambia");
+$connection = mysqli_connect("mkorvuw3sl6cu9ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","iigi5330azzahj3c","smij1gf9l7l65kwi","jj0m1ku5w7a37l39");
 
 
 if(isset($_POST['updatecashcol']))
