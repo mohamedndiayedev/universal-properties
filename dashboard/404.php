113cd67195fdc3a52c1ec3056e9b86e25f8eaeb5
Mohamed Ndiaye <38785512+mohamedndiayedev@users.noreply.github.com>
2020-08-11 04:13:03 +0100

    Update 404.php

--- dashboard/404.php
+++ dashboard/404.php
@@ -9,8 +9,8 @@ include('includes/header.php');
             <div class="card-body">
 <h1 class="card-title bg-warning"><span style="color:red;"> Error page</span> </h1>
 <h2 class="card-title"> 404 page </h2>
-<p class="card-text">La page client est en cours de construction! <br/><span style="color:#b2dd4c;"> Codou k vous remercie.</span></p>
-<a href="../index.php" class="btn btn-primary">Retour Accueil</a>
+<p class="card-text">This page is under construction! <br/><span style="color:#b2dd4c;"> Contact NDIAYE Mohamed: jaayahmad@gmail.com.</span></p>
+<a href="../index.php" class="btn btn-primary">Go back Dashboard</a>
     
             </div>
         </div>
